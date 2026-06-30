f0865091f61f5e5225006856ba477216e9581768
Saurabh Sanwal <saurabhsanwal01@gmail.com>
2026-06-30 16:46:29 +0530

    fix backend and css

--- blog-backend/controllers/post.controller.js
+++ blog-backend/controllers/post.controller.js
@@ -3,6 +3,7 @@ import Post from "../models/Post.model.js"
 export const getAllPosts = async (req, res) => {
   try {
     const posts = await Post.find()
+    console.log(posts)
     res.status(200).json(posts)
   } catch (error) {
     res.status(500).json({ message: error.message })
